11d1163ef81da933368c4bee7c0a81077299b665
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2024-03-15 17:07:13 +0700

    Inintial Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
